2a8282579e069f4afb177bebced3d47509b00d81
Saull Brandão <saull@outlook.com>
2023-04-15 08:29:52 -0300

    initial commit

---
+++ src/main/java/com/labmedicine/labmedicalapi/LabmedicalApiApplication.java
@@ -0,0 +1,13 @@
+package com.labmedicine.labmedicalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabmedicalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabmedicalApiApplication.class, args);
+	}
+
+}
